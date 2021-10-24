1d6ed8973b60ea97101a2ae031a5f6c16ce7f96e
EvgeniiSamarin <evgeniy.samarin@flatstack.com>
2021-10-24 19:23:09 +0300

    Add arrays usage example

--- Homework3/Homework3/ViewController.swift
+++ Homework3/Homework3/ViewController.swift
@@ -11,7 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
+        var array = Array<Int>(capacity: 10)
+        array.push(element: 21)
+        array.pop()
+
+        array.push(element: 13)
+        array.push(element: 14)
+        array.push(element: 15)
+        print(array)
+        array.pop()
+        print(array)
     }
 
 
